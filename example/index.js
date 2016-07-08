997750d2fbcc43fcb63f73166b9ef4049f62413b
Ionică Bizău <bizauionica@gmail.com>
2016-07-08 06:50:24 +0300

    Added example

--- example/index.js
+++ example/index.js
@@ -3,3 +3,13 @@
 const smartFlatColor = require("../lib");
 
 console.log(smartFlatColor());
+// => #ecf0f1
+
+console.log(smartFlatColor(["orange", "red"]));
+// => #e67e22
+
+console.log(smartFlatColor("green"));
+// => #7f8c8d
+
+console.log(smartFlatColor("white,gray"));
+// => #27ae60
